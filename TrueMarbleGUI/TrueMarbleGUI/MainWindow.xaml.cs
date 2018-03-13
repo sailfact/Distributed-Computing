01ff9580833890b401d167ebaeefd9e27124bab5
sailfact <33059335+sailfact@users.noreply.github.com>
2018-03-13 21:47:34 +0800

    started gui

--- TrueMarbleGUI/TrueMarbleGUI/MainWindow.xaml.cs
+++ TrueMarbleGUI/TrueMarbleGUI/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.ServiceModel;
+﻿using System;
+using System.ServiceModel;
 using System.Windows;
+using System.Windows.Media.Imaging;
+using System.IO;
 using TrueMarbleData;
 
 namespace TrueMarbleGUI
@@ -14,20 +17,46 @@ namespace TrueMarbleGUI
         public MainWindow()
         {
             InitializeComponent();
-        }     
+        }
 
-        private void BtnLoad_Loaded(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             ChannelFactory<ITMDataController> channelFactory;
+            JpegBitmapDecoder decoder;
+            Stream memoryStream;
 
             NetTcpBinding tcpBinding = new NetTcpBinding();
             string url = "net.tcp://localhost:50001/TMData";
 
-            channelFactory = new ChannelFactory<ITMDataController>(tcpBinding, url);
+            try
+            {
+                channelFactory = new ChannelFactory<ITMDataController>(tcpBinding, url);
 
-            m_tmData = channelFactory.CreateChannel();
+                m_tmData = channelFactory.CreateChannel();
 
-            m_tmData.LoadTile(4, 0,0);
+                memoryStream = new MemoryStream(m_tmData.LoadTile(4, 0, 0));
+            }
+            catch (ArgumentNullException ae)
+            {
+                throw new FaultException(ae.Message);
+            }
+
+            try
+            {
+                decoder = new JpegBitmapDecoder(memoryStream, BitmapCreateOptions.None, BitmapCacheOption.None);
+            }
+            catch (FileFormatException fe)
+            {
+                throw new FaultException(fe.Message);
+            }
+
+            imgTile.Source = decoder.Frames[0];
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
+ 
